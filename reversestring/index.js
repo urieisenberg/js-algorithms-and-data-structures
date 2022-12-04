4954c0d166be279c8ed233d2708c02f04ac64741
urieisenberg <urieisenberg@gmail.com>
2022-12-04 18:14:35 +0200

    string reversal - solution two

--- reversestring/index.js
+++ reversestring/index.js
@@ -6,8 +6,18 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
+// Solution 1
+// function reverse(str) {
+//   return str.split('').reverse().join('');
+// }
+
+// Solution 2
 function reverse(str) {
-  return str.split('').reverse().join('');
+  let reversed = '';
+  for (let character of str) {
+    reversed = character + reversed;
+  }
+  return reversed;
 }
 
 module.exports = reverse;
