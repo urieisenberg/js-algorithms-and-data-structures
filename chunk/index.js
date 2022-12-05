a6713826a692b28d1685418b75b75f1a94731d02
urieisenberg <urieisenberg@gmail.com>
2022-12-05 22:34:58 +0200

    Chunk solution 1

--- chunk/index.js
+++ chunk/index.js
@@ -8,6 +8,20 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  //Solution 1
+  
+
+  //Solution 2
+  const chunked = [];
+  let index = 0;
+
+  while (index < array.length) {
+    chunked.push(array.slice(index, index + size));
+    index += size;
+  }
+
+  return chunked;
+}
 
 module.exports = chunk;
