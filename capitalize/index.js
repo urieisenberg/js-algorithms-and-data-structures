61769205e343be9f2b65d6311be5cf9fdc332cb1
urieisenberg <urieisenberg@gmail.com>
2022-12-09 13:20:03 +0200

    capitalize solution 2

--- capitalize/index.js
+++ capitalize/index.js
@@ -8,15 +8,23 @@
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
 function capitalize(str) {
-  let result = str[0].toUpperCase();
-  for (let i = 1; i < str.length; i++) {
-    if (str[i - 1] === ' ') {
-      result += str[i].toUpperCase();
-    } else {
-      result += str[i];
-    }
+  // Solution #1
+  //   let result = str[0].toUpperCase();
+  //   for (let i = 1; i < str.length; i++) {
+  //     if (str[i - 1] === ' ') {
+  //       result += str[i].toUpperCase();
+  //     } else {
+  //       result += str[i];
+  //     }
+  //   }
+  //   return result;
+
+  // Solution #2
+  const words = [];
+  for (let word of str.split(' ')) {
+    words.push(word[0].toUpperCase() + word.slice(1));
   }
-  return result;
+  return words.join(' ');
 }
 
 module.exports = capitalize;
