e6ba8f65577ca63f88649faa64f46d31dbb1a550
urieisenberg <urieisenberg@gmail.com>
2022-12-09 13:31:57 +0200

    printing steps solution 1

--- steps/index.js
+++ steps/index.js
@@ -17,6 +17,25 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n) {
+  // Solution #1
+  // from 0 to n (iterate through rows) ->
+  // create an empty string, 'stair' ->
+  // from 0 to n (iterate through columns) ->
+  // if the current column is equal to or less than the current row ->
+  // add a '#' to 'stair', else add a space to 'stair' ->
+  // console.log 'stair'
+  for (let row = 0; row < n; row++) {
+    let stair = '';
+    for (let column = 0; column < n; column++) {
+      if (column <= row) {
+        stair += '#';
+      } else {
+        stair += ' ';
+      }
+    }
+    console.log(stair);
+  }
+}
 
 module.exports = steps;
