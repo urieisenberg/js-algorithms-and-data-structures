3a8afa48765649f84975c49aa81ae9f270e6df47
urieisenberg <urieisenberg@gmail.com>
2022-12-11 14:36:49 +0200

    pyramid solution 2 - recursion

--- pyramid/index.js
+++ pyramid/index.js
@@ -14,7 +14,6 @@
 //       ' ### '
 //       '#####'
 
-// Solution #1
 // from 0 to n (iterate through rows) ->
 // create an empty string, 'level' ->
 // from 0 to ??? (columns) ->
@@ -23,20 +22,37 @@
 // else ->
 // add a space to 'level' ->
 // console.log 'stair'
-//
-function pyramid(n) {
-  const midpoint = Math.floor((2 * n - 1) / 2);
-  for (let row = 0; row < n; row++) {
-    let level = '';
-    for (let column = 0; column < 2 * n - 1; column++) {
-      if (midpoint - row <= column && midpoint + row >= column) {
-        level += '#';
-      } else {
-        level += ' ';
-      }
-    }
+//// Solution #1
+// function pyramid(n) {
+//   const midpoint = Math.floor((2 * n - 1) / 2);
+//   for (let row = 0; row < n; row++) {
+//     let level = '';
+//     for (let column = 0; column < 2 * n - 1; column++) {
+//       if (midpoint - row <= column && midpoint + row >= column) {
+//         level += '#';
+//       } else {
+//         level += ' ';
+//       }
+//     }
+//     console.log(level);
+//   }
+// }
+
+// Solution #2
+function pyramid(n, row = 0, level = '') {
+  if (row === n) return;
+  if (level.length === 2 * n - 1) {
     console.log(level);
+    return pyramid(n, row + 1);
   }
+  const midpoint = Math.floor((2 * n - 1) / 2);
+  let add;
+  if (midpoint - row <= level.length && midpoint + row >= level.length) {
+    add = '#';
+  } else {
+    add = ' ';
+  }
+  pyramid(n, row, level + add);
 }
 
 module.exports = pyramid;
