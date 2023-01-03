f381c58344fa78d18e175c93c78885ae363cbbd6
urieisenberg <urieisenberg@gmail.com>
2023-01-03 15:14:30 +0200

    finding vowels with regex solution

--- vowels/index.js
+++ vowels/index.js
@@ -9,14 +9,18 @@
 
 function vowels(str) {
   //solution #1
-  let count = 0;
-  const checker = ['a', 'e', 'i', 'o', 'u'];
-  for (let char of str.toLowerCase()) {
-    if (checker.includes(char)) {
-      count++;
-    }
-  }
-  return count;
+  //   let count = 0;
+  //   const checker = ['a', 'e', 'i', 'o', 'u'];
+  //   for (let char of str.toLowerCase()) {
+  //     if (checker.includes(char)) {
+  //       count++;
+  //     }
+  //   }
+  //   return count;
+
+  //solution #2 //regex solution
+  const matches = str.match(/[aeiou]/gi);
+  return matches ? matches.length : 0;
 }
 
 module.exports = vowels;
