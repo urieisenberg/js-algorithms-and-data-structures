9e2a744048048c4fccdbf9feb520b7740cb91834
urieisenberg <urieisenberg@gmail.com>
2023-01-03 15:22:34 +0200

    matrix spiral solution

--- matrix/index.js
+++ matrix/index.js
@@ -15,10 +15,8 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
 
-
-//notes for solution: 
+//notes for solution:
 //create empty array of arrays called 'results'
 //create a counter variable, starting at 1
 //as long as (start column <= end column) AND (start row <= end row)
@@ -31,6 +29,51 @@ function matrix(n) {}
 //increment counter
 //decrement end column
 //...repeat for other two sides
+
+function matrix(n) {
+  const results = [];
+  for (let i = 0; i < n; i++) {
+    results.push([]);
+  }
+
+  let counter = 1;
+  let startColumn = 0;
+  let endColumn = n - 1;
+  let startRow = 0;
+  let endRow = n - 1;
+
+  while (startColumn <= endColumn && startRow <= endRow) {
+    //top row
+    for (let i = startColumn; i <= endColumn; i++) {
+      results[startRow][i] = counter;
+      counter++;
+    }
+    startRow++;
+
+    //right column
+    for (let i = startRow; i <= endRow; i++) {
+      results[i][endColumn] = counter;
+      counter++;
+    }
+    endColumn--;
+
+    //bottom row
+    for (let i = endColumn; i >= startColumn; i--) {
+      results[endRow][i] = counter;
+      counter++;
+    }
+    endRow--;
+
+    //start column
+    for (let i = endRow; i >= startRow; i--) {
+      results[i][startColumn] = counter;
+      counter++;
+    }
+    startColumn++;
+  }
+  return results;
+}
+
 
 
 module.exports = matrix;
