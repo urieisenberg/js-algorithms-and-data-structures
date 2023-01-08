89243079088c5e017ed04c213adb3df492fea008
urieisenberg <urieisenberg@gmail.com>
2023-01-08 15:52:15 +0200

    validation binary search solution

--- validate/index.js
+++ validate/index.js
@@ -5,6 +5,14 @@
 // every node's right hand child is greater than
 // the parent
 
-function validate(node, min = null, max = null) {}
+function validate(node, min = null, max = null) {
+  if (max !== null && node.data > max) return false; // if max is not null and node.data is greater than max, return false
+  if (min !== null && node.data < min) return false; // if min is not null and node.data is less than min, return false
+
+  if (node.left && !validate(node.left, min, node.data)) return false; // if node.left is not null and validate returns false, return false
+  if (node.right && !validate(node.right, node.data, max)) return false; // if node.right is not null and validate returns false, return false
+
+  return true; // if all conditions are met, return true
+}
 
 module.exports = validate;
