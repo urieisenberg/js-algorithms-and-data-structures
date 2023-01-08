407bdd2ae6259fafeae123b8007c9c8482df1f67
urieisenberg <urieisenberg@gmail.com>
2023-01-08 15:59:37 +0200

    Events solution

--- events/index.js
+++ events/index.js
@@ -8,15 +8,34 @@ class Events {
     this.events = {};
   }
   // Register an event handler
-  on(eventName, callback) {}
+  on(eventName, callback) {
+    // on is a method that takes in an event name and a callback
+    if (this.events[eventName]) {
+      // if the event name exists in the events object
+      this.events[eventName].push(callback); // push the callback into the array
+    } else {
+      // if the event name does not exist in the events object
+      this.events[eventName] = [callback]; // create a new array with the callback
+    }
+  }
 
   // Trigger all callbacks associated
   // with a given eventName
-  trigger(eventName) {}
+  trigger(eventName) {
+    if (this.events[eventName]) {
+      // if the event name exists in the events object
+      for (let cb of this.events[eventName]) {
+        // iterate through the array of callbacks
+        cb(); // invoke the callback
+      }
+    }
+  }
 
   // Remove all event handlers associated
   // with the given eventName
-  off(eventName) {}
+  off(eventName) {
+    delete this.events[eventName]; // delete the event name from the events object
+  }
 }
 
 module.exports = Events;
