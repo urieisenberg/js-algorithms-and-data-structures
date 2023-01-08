4a7ca1d6e2fae0de4dc1f447d274e8c51272ec6b
urieisenberg <urieisenberg@gmail.com>
2023-01-08 16:16:25 +0200

    bubbleSort algorithm solution O(n^2)

--- sorting/index.js
+++ sorting/index.js
@@ -2,19 +2,26 @@
 // Implement bubbleSort, selectionSort, and mergeSort
 
 function bubbleSort(arr) {
+  //iterate through the array
+  for (let i = 0; i < arr.length; i++) {
+    //iterate through the array again
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      //if the first element is greater than the second element
+      if (arr[j] > arr[j + 1]) {
+        //swap the two elements
+        let lesser = arr[j + 1];
+        arr[j + 1] = arr[j];
+        arr[j] = lesser;
+      }
+    }
+  }
+  return arr;
+} // O(n^2) time complexity
 
-}
+function selectionSort(arr) {}
 
-function selectionSort(arr) {
+function mergeSort(arr) {}
 
-}
-
-function mergeSort(arr) {
-
-}
-
-function merge(left, right) {
-
-}
+function merge(left, right) {}
 
 module.exports = { bubbleSort, selectionSort, mergeSort, merge };
